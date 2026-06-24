0cebc47ac9e60d1d96baca17b4369ad28e2ffde1
leegatuso <leegatuso@gmail.com>
2026-06-24 20:52:59 +0900

    Initial cp-dashboard project

---
+++ src/main/java/com/company/cpdashboard/CpDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.company.cpdashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpDashboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CpDashboardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/company/cpdashboard/CpDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.cpdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpDashboardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
